c4e2354b18fe4f6c9fdf3e78089dfc90320f6b45
VietNQV <vietnq191@gmail.com>
2022-05-22 06:59:00 +0700

    all screen

---
+++ MangaOwl/MangaOwl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MangaOwl
+//
+//  Created by CNTT on 5/17/22.
+//  Copyright © 2022 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
